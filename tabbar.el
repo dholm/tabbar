f90898538b54b53602f845940f00337e3d30bb04
David Holm <david.holm@ericsson.com>
2011-08-24 14:39:04 +0200

    Only enable tabbar-mwheel-follow if mouse-wheel-mode is actually available

--- tabbar.el
+++ tabbar.el
@@ -1613,7 +1613,8 @@ Returns non-nil if the new state is enabled.
 
 (defun tabbar-mwheel-follow ()
   "Toggle Tabbar-Mwheel following Tabbar and Mouse-Wheel modes."
-  (tabbar-mwheel-mode (if (and mouse-wheel-mode tabbar-mode) 1 -1)))
+  (if (boundp 'mouse-wheel-mode)
+      (tabbar-mwheel-mode (if (and mouse-wheel-mode tabbar-mode) 1 -1))))
 
 (add-hook 'tabbar-mode-hook      'tabbar-mwheel-follow)
 (add-hook 'mouse-wheel-mode-hook 'tabbar-mwheel-follow)
